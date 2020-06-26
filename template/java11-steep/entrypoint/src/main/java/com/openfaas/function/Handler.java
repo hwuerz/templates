728d44169d5370adad5ddca06c080ab4a6a5b0a8
hwuerz <h.m.wuerz@web.de>
2020-06-26 12:05:08 +0200

    Fixed jackson error for kotlin data classes

--- template/java11-steep/entrypoint/src/main/java/com/openfaas/function/Handler.java
+++ template/java11-steep/entrypoint/src/main/java/com/openfaas/function/Handler.java
@@ -2,6 +2,7 @@ package com.openfaas.function;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.kotlin.KotlinModule;
 import com.openfaas.model.IHandler;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.IResponse;
@@ -12,6 +13,11 @@ public class Handler implements IHandler {
 
     io.steep.faas.Handler steepHandler = new io.steep.faas.Handler();
     ObjectMapper objectMapper = new ObjectMapper();
+
+    public Handler() {
+        // Support for Kotlin data class.
+        objectMapper.registerModule(new KotlinModule());
+    }
 
     public IResponse Handle(IRequest req) {
 
@@ -28,14 +34,17 @@ public class Handler implements IHandler {
                 res.setBody(steepResponse);
 
             } catch (Exception e) { // Function code failed.
+                e.printStackTrace();
                 res.setStatusCode(500);
                 res.setBody(e.getMessage());
             }
         } catch (JsonProcessingException e) { // Input JSON mapping failed.
+            e.printStackTrace();
             res.setStatusCode(400);
             res.setBody("Body does not contain an Executable object: " + e.getMessage() +
                     " You passed " + req.getBody());
         } catch (Exception e) { // Should never happen.
+            e.printStackTrace();
             res.setStatusCode(500);
             res.setBody("Unknown Error: " + e.getMessage());
         }
